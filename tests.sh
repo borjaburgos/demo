44377900870fa2c14348a3a58e1a97c50dbce603
Borja Burgos <borja@tutum.co>
2015-07-07 19:26:56 -0400

    fixing issues

--- tests.sh
+++ tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 python app.py > /dev/null &
 sleep 5
-if curl http://localhost:80 | grep -q '<b>Visits:</b> 3<br/>'; then
+if curl http://localhost:80 | grep -q '<b>Visits:</b> 1<br/>'; then
   exit 0
 fi
-echo -e "\e[41m--------------"
+echo -e "\e[41m-------------"
 echo -e "\e[91m Tests failed"
-echo -e "\e[41m--------------"
+echo -e "\e[41m-------------"
 exit 1
