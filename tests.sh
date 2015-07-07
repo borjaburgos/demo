25f8dae087b9798eb1db58448b0d95e2d4d79ff1
Borja Burgos <borja@tutum.co>
2015-07-07 19:22:41 -0400

    changed colors in hooks and tests.sh

--- tests.sh
+++ tests.sh
@@ -4,4 +4,7 @@ sleep 5
 if curl http://localhost:80 | grep -q '<b>Visits:</b> 3<br/>'; then
   exit 0
 fi
+echo -e "\e[41m--------------"
+echo -e "\e[91m Tests failed"
+echo -e "\e[41m--------------"
 exit 1
