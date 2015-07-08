d4510d5cb83cb45cdd842da62f43a7333c3964c3
Borja Burgos <borja@tutum.co>
2015-07-08 12:18:00 -0400

    yet another test

--- tests.sh
+++ tests.sh
@@ -2,9 +2,9 @@
 python app.py > /dev/null &
 sleep 5
 if curl http://localhost:80 | grep -q '<b>Visits:</b> 1<br/>'; then
-  echo -e "\e[42m---------------"
+  echo -e "\e[42m--------------"
   echo -e "\e[42m-\e[0m\e[92mTests passed\e[0m\e[42m-"
-  echo -e "\e[42m---------------"
+  echo -e "\e[42m--------------"
   exit 0
 fi
 echo -e "\e[41m-------------"
