a2566dc4b8364fe5197320b152782f722c3b4096
Borja Burgos <borja@tutum.co>
2015-07-09 12:17:24 -0400

    changing to Hola instead of Hello

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>counter disabled. Cannot connect to Redis.</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
+    html = "<h3>Hola {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}<br/>"
 #           "<img src=http://bit.ly/1Cry9PX>"
